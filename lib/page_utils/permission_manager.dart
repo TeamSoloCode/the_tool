d7340a7367a37ea06874e8e2a2375637df765b46
BruceBui5041 <brucebui5041@gmail.com>
2022-07-07 23:45:35 +0700

    Update permission_manager

--- lib/page_utils/permission_manager.dart
+++ lib/page_utils/permission_manager.dart
@@ -1,6 +1,40 @@
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionManager {
+  static Map<String, int> permissionValue = {
+    "calendar": 0,
+    "camera": 1,
+    "contacts": 2,
+    "location": 3,
+    "locationAlways": 4,
+    "locationWhenInUse": 5,
+    "mediaLibrary": 6,
+    "microphone": 7,
+    "phone": 8,
+    "photos": 9,
+    "photosAddOnly": 10,
+    "reminders": 11,
+    "sensors": 12,
+    "sms": 13,
+    "speech": 14,
+    "storage": 15,
+    "ignoreBatteryOptimizations": 16,
+    "notification": 17,
+    "accessMediaLocation": 18,
+    "activityRecognition": 19,
+    "unknown": 20,
+    "bluetooth": 21,
+    "manageExternalStorage": 22,
+    "systemAlertWindow": 23,
+    "requestInstallPackages": 24,
+    "appTrackingTransparency": 25,
+    "criticalAlerts": 26,
+    "accessNotificationPolicy": 27,
+    "bluetoothScan": 28,
+    "bluetoothAdvertise": 29,
+    "bluetoothConnect": 30,
+  };
+
   Future<PermissionStatus?> requestPermission(String permissionName) async {
     var permission = _getPermissionByName(permissionName);
     return permission?.request();
@@ -11,12 +45,10 @@ class PermissionManager {
     return Future.value(permission?.status);
   }
 
-  PermissionWithService? _getPermissionByName(String permissionName) {
-    switch (permissionName) {
-      case 'location':
-        return Permission.location;
-      default:
-        return null;
+  Permission? _getPermissionByName(String permissionName) {
+    if (permissionValue.containsKey(permissionName)) {
+      return Permission.byValue(permissionValue[permissionName]!);
     }
+    return null;
   }
 }
