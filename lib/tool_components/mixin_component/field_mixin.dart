818e1cd45096865bc03f05ca41818e2f297e7067
BruceBui5041 <brucebui5041@gmail.com>
2022-09-19 19:28:37 +0700

    Add more field validators

--- lib/tool_components/mixin_component/field_mixin.dart
+++ lib/tool_components/mixin_component/field_mixin.dart
@@ -1,10 +1,14 @@
 import 'dart:developer';
 
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:the_tool/utils.dart';
 
 mixin FieldMixin {
+  final UtilsManager _utils = UtilsManager();
+
   List<String? Function(String?)> computeValidators(
     List<Map<String, dynamic>>? validatorsMap,
+    Map<String, dynamic> contextData,
   ) {
     List<String? Function(String?)> validators = [];
     validatorsMap?.forEach((validator) {
@@ -20,9 +24,58 @@ mixin FieldMixin {
         );
       }
 
-      if (validator["type"] == "is_email") {
+      if (validator["type"] == "max") {
+        dynamic maxValue;
+
+        if (UtilsManager.isValueBinding(validator["maxValue"].toString())) {
+          maxValue = _utils.bindingValueToProp(
+            contextData,
+            validator["maxValue"],
+          );
+          maxValue = double.tryParse(maxValue.toString());
+        } else {
+          maxValue = double.tryParse(validator["maxValue"].toString());
+        }
+
+        if (maxValue == null) {
+          throw Exception("maxValue of max validator must be a number");
+        }
+
+        var inclusive = UtilsManager.isTruthy(validator["inclusive"]);
+
         validators.add(
-          FormBuilderValidators.email(errorText: validator["errorText"]),
+          FormBuilderValidators.max(
+            maxValue,
+            inclusive: inclusive,
+            errorText: validator["errorText"],
+          ),
+        );
+      }
+
+      if (validator["type"] == "min") {
+        dynamic minValue;
+        if (UtilsManager.isValueBinding(validator["maxValue"].toString())) {
+          minValue = _utils.bindingValueToProp(
+            contextData,
+            validator["maxValue"],
+          );
+          minValue = double.tryParse(minValue.toString());
+        } else {
+          minValue = double.tryParse(validator["minValue"].toString());
+        }
+
+        if (minValue == null) {
+          throw Exception("minValue of min validator must be a number");
+        }
+
+        var inclusive = UtilsManager.isTruthy(validator["inclusive"]);
+
+        validators.add(
+          FormBuilderValidators.min(
+            minValue,
+            inclusive: inclusive,
+            errorText: validator["errorText"],
+          ),
         );
       }
     });
