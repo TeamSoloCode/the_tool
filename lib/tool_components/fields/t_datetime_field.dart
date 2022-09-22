5fc23cb3833836aebd845254583a775a5ff2fdeb
BruceBui5041 <brucebui5041@gmail.com>
2022-09-23 00:06:24 +0700

    Update t_datetime and pubspec.yaml

--- lib/tool_components/fields/t_datetime_field.dart
+++ lib/tool_components/fields/t_datetime_field.dart
@@ -7,6 +7,7 @@ import 'package:the_tool/constants.dart';
 import 'package:the_tool/t_widget_interface/layout_content/layout_props.dart';
 import 'package:the_tool/tool_components/mixin_component/field_mixin.dart';
 import 'package:the_tool/tool_components/t_widget.dart';
+import 'package:the_tool/utils.dart';
 
 class T_Datetime extends TWidget {
   T_Datetime({
@@ -31,7 +32,7 @@ class _T_DatetimeState extends TStatefulWidget<T_Datetime> with FieldMixin {
   final _datetimeKey = GlobalKey<FormBuilderFieldState>();
   String? _errorMessage;
   DateTime? selectedValue;
-  var datetimeFieldController = TextEditingController();
+  var _datetimeFieldController = TextEditingController();
 
   @override
   void initState() {
@@ -45,7 +46,9 @@ class _T_DatetimeState extends TStatefulWidget<T_Datetime> with FieldMixin {
     String? name = widget.widgetProps.name;
     dynamic currentValue = _datetimeKey.currentState?.value;
     try {
-      selectedValue = DateTime.parse(widget.contextData[name].toString());
+      selectedValue = widget.contextData[name] != null
+          ? DateTime.parse(widget.contextData[name].toString())
+          : null;
     } catch (e) {
       throw Exception(
         "Fail to parse ${widget.contextData[name].toString()}. Please use \"yyyy-MM-dd hh:mm:ss\" format",
@@ -55,10 +58,9 @@ class _T_DatetimeState extends TStatefulWidget<T_Datetime> with FieldMixin {
     if (selectedValue.toString() != currentValue.toString() && name != null) {
       Future.delayed(Duration.zero, () async {
         _datetimeKey.currentState?.setValue(selectedValue);
-        datetimeFieldController.text = selectedValue != null
+        _datetimeFieldController.text = selectedValue != null
             ? DateFormat(DefaultDateFormat).format(selectedValue!)
             : "";
-        _datetimeKey.currentState?.setState(() {});
       });
     }
 
@@ -76,6 +78,25 @@ class _T_DatetimeState extends TStatefulWidget<T_Datetime> with FieldMixin {
     selectedValue = value;
   }
 
+  Widget? _getSuffixIcon() {
+    if (UtilsManager.isTruthy(
+      widget.props?.allowClear,
+    )) {
+      return IconButton(
+        onPressed: () {
+          _datetimeKey.currentState?.setValue(null);
+          _datetimeFieldController.text = "";
+          _onChangeDate(null);
+        },
+        icon: const Icon(
+          Icons.close,
+        ),
+      );
+    }
+
+    return null;
+  }
+
   Widget _computeDatetimeField(
     LayoutProps? widgetProps,
     Map<String, dynamic> contextData,
@@ -86,13 +107,14 @@ class _T_DatetimeState extends TStatefulWidget<T_Datetime> with FieldMixin {
     assert(name != null, "Missing \"name\" in field widget");
     return FormBuilderDateTimePicker(
       key: _datetimeKey,
-      controller: datetimeFieldController,
+      controller: _datetimeFieldController,
       name: name ?? "",
       format: DateFormat(DefaultDateFormat),
       locale: Localizations.localeOf(context),
       decoration: computeFieldDecoration(
         widgetProps,
         errorMessage: _errorMessage,
+        suffixIcon: _getSuffixIcon(),
       ),
       initialValue: DateTime.tryParse(
         (widget.props?.defaultValue ?? value).toString(),
