81c16af80012a43e77a8ea7f9eaff143776850cc
Bui Hong Lan <lan@hydrax.io>
2023-07-27 21:30:02 +0700

    update form field integration test

--- integration_test/form_fields_test.dart
+++ integration_test/form_fields_test.dart
@@ -25,57 +25,74 @@ void main() async {
 
     await Future.delayed(const Duration(seconds: 1));
 
-    var emailField = find.byType(FormBuilderTextField).first;
-
-    await widgetTester.pumpAndSettle();
-    await widgetTester.enterText(emailField, 'abcd@gmail.com');
-    await widgetTester.pumpAndSettle();
-
-    var formEmailField = widgetTester.widget<FormBuilderTextField>(emailField);
-
-    await Future.delayed(const Duration(seconds: 1));
-
-    expect(formEmailField.enabled, true);
-
-    expect(
-      formEmailField.decoration.errorText,
-      "User already existed !!!",
+    // abcd@gmail is invalid email so it will show error
+    // "User already existed !!!" is coming from validateFunction in layout code
+    await inputEmail(
+      widgetTester,
+      email: "abcd@gmail.com",
+      expectedErrorText: "User already existed !!!",
     );
 
-    var passwordField = find.byType(FormBuilderTextField).last;
-    var formPasswordField =
-        widgetTester.widget<FormBuilderTextField>(passwordField);
-
-    await widgetTester.enterText(passwordField, '123');
-    await widgetTester.pumpAndSettle(const Duration(milliseconds: 500));
-
-    expect(
-      formPasswordField.decoration.errorText,
-      null,
+    // 123 is invalid password but it's not checked yet because email still is invalid
+    await inputPassword(
+      widgetTester,
+      password: "123",
+      expectedErrorText: null,
     );
 
-    await widgetTester.enterText(emailField, 'abcd@gmail1.com');
-    await widgetTester.pumpAndSettle(const Duration(milliseconds: 500));
-
-    formEmailField = widgetTester.widget<FormBuilderTextField>(emailField);
-
-    await Future.delayed(const Duration(seconds: 1));
-
-    expect(
-      formEmailField.decoration.errorText,
-      null,
+    // abcd@gmail1.com is valid email
+    await inputEmail(
+      widgetTester,
+      email: "abcd@gmail1.com",
+      expectedErrorText: null,
     );
 
-    await widgetTester.enterText(passwordField, '1234');
-    await widgetTester.pumpAndSettle(const Duration(milliseconds: 500));
-    formPasswordField =
-        widgetTester.widget<FormBuilderTextField>(passwordField);
-
-    await Future.delayed(const Duration(seconds: 1));
-
-    expect(
-      formPasswordField.decoration.errorText,
-      "Password is too short !!!",
+    // 1234 is valid password but it's so error because email now is valid
+    await inputPassword(
+      widgetTester,
+      password: "1234",
+      expectedErrorText: "Password is too short !!!",
     );
   });
+}
+
+Future<void> inputEmail(
+  WidgetTester widgetTester, {
+  required String email,
+  String? expectedErrorText,
+}) async {
+  var emailField = find.byType(FormBuilderTextField).first;
+
+  await widgetTester.pumpAndSettle();
+  await widgetTester.enterText(emailField, email);
+  await widgetTester.pumpAndSettle();
+
+  var formEmailField = widgetTester.widget<FormBuilderTextField>(emailField);
+
+  await Future.delayed(const Duration(seconds: 1));
+
+  expect(
+    formEmailField.decoration.errorText,
+    expectedErrorText,
+  );
+}
+
+Future<void> inputPassword(
+  WidgetTester widgetTester, {
+  required String password,
+  String? expectedErrorText,
+}) async {
+  var passwordField = find.byType(FormBuilderTextField).last;
+
+  await widgetTester.enterText(passwordField, password);
+  await widgetTester.pumpAndSettle(const Duration(milliseconds: 500));
+
+  var formPasswordField =
+      widgetTester.widget<FormBuilderTextField>(passwordField);
+
+  await Future.delayed(const Duration(seconds: 1));
+  expect(
+    formPasswordField.decoration.errorText,
+    expectedErrorText,
+  );
 }
